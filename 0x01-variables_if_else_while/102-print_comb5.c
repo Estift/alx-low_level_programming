a8707ff07a8fa65a0caa1b8f0afa763bba039a71
Estift <estifanos.tassew1@gmail.com>
2022-07-01 11:41:07 -0700

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,19 +10,24 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = 0; num2 <= 98; num1++)
+	for (num1 = 0; num2 < 100; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (num2 = 0; num2 < 100; num2++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if (num1 == 98 && num2 == 99) continue;
-			putchar(',');
-			putchar(' ');
+			if (num1 < num2)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+				
+				if (num1 != 98 && num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
