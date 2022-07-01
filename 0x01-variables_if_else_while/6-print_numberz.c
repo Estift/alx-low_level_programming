9a6bbeaeff07b4f22592d5db7516689afe9cea8c
Estift <estifanos.tassew1@gmail.com>
2022-07-01 05:29:22 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print 123456789 without using char or printf
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
