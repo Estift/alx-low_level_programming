cbf36bee8dd52b1dc547ece9fd8e2d6f65c7f64d
Estift <estifanos.tassew1@gmail.com>
2022-07-01 11:55:38 -0700

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,9 +13,9 @@ int main(void)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-			for (t = tens; t<= '9'; t++)
+			for (t = tens; t <= '9'; t++)
 			{
-				for (o = ones + 1; o <= '9'; o++)
+				for (o = o + 1; o <= '9'; o++)
 				{
 					putchar(tens);
 					putchar(ones);
