147e09c837500dd34f91e8ab49512d3dc47e1184
Estift <estifanos.tassew1@gmail.com>
2022-07-05 09:00:05 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,15 @@
 
 int main(void)
 {
-	_putchar('_', 'p', 'u', 't', 'c', 'h', 'a', 'r');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
 	return (0);
 }
