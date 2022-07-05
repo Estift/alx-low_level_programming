af93c3499f475ef9c1fe2aeb4477a8eaf7df20a6
Estift <estifanos.tassew1@gmail.com>
2022-07-05 13:02:21 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * time_table - print multiplication table
+ * times_table - print multiplication table
  */
 
-void time_table(void)
+void times_table(void)
 {
 	int row;
 	int column;
@@ -27,7 +27,7 @@ void time_table(void)
 				_putchar(' ');
 				_putchar('0' + product);
 			}
-			else if (product > =)
+			else if (product > 9)
 			{
 				_putchar(',');
 				_putchar(' ');
