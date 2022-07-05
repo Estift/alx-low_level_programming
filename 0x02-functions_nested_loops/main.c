0f076cd6fe58f758e619e2802ca9180aefc5ce05
Estift <estifanos.tassew1@gmail.com>
2022-07-05 08:43:45 -0700

    a

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,18 @@
+#ifndef _main_h_
+#define _main_h_
+
+/**
+ * _putchar -writes on character
+ *
+ *
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+void print_alphabet(void)
+
+/**
+ *
