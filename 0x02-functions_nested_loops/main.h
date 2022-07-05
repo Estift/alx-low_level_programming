a491d787ca703e1db5adf1f3599418fc10e222ea
Estift <estifanos.tassew1@gmail.com>
2022-07-05 11:28:41 -0700

    a

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,7 +17,7 @@ int _putchar(char c);
 void print_alphabet(void);
 
 /**
- * print_alphabet_x10 - print lowercase 
+ * print_alphabet_x10 - print lowercase alphabet ten times 
  */
 
 void print_alphabet_x10(void);
