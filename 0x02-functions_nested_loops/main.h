c4ccf924989f53cbf2d0b55f1aacca4b2f46c20f
Estift <estifanos.tassew1@gmail.com>
2022-07-05 09:21:59 -0700

    a

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,11 @@
 #define _main_h_
 
 /**
- * _putchar -writes on character
+ * _putchar -writes the character c to stdout
+ * @c: The character to print
  *
- *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
  */
 
 int _putchar(char c);
@@ -12,7 +14,11 @@ int _putchar(char c);
 /**
  * print_alphabet - print lowercase alphabet
  */
-void print_alphabet(void)
+void print_alphabet(void);
 
 /**
- *
+ * print_alphabet_x10 - print lowercase 
+ */
+
+void print_alphabet_x10(void);
+
